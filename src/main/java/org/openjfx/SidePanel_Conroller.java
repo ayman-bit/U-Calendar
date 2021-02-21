1b30cf239815c48bf9240779cdadee1a963868ca
mosh-exe <mohammeds@mun.ca>
2021-02-21 02:08:01 -0330

    Added simple add event window
    Signed-off-by: mosh-exe <mohammeds@mun.ca>

--- src/main/java/org/openjfx/SidePanel_Conroller.java
+++ src/main/java/org/openjfx/SidePanel_Conroller.java
@@ -1,9 +1,21 @@
 package org.openjfx;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+
+
 
 /*
 @Author Ayman Abu Awad
@@ -18,6 +30,21 @@ public class SidePanel_Conroller {
 
     @FXML
     void initialize() {
+
+    }
+
+    @FXML
+    private JFXButton addEvent;
+
+    @FXML
+    void handleAdd(MouseEvent event) throws IOException {
+
+        Parent mainApp = FXMLLoader.load(getClass().getResource("AddEvent.fxml"));
+        Scene mainScene = new Scene(mainApp);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setTitle("Add Event");
+        window.setScene(mainScene);
+        window.show();
 
     }
 
