8581e8ca973ebf14129b7ef22e0ca293a119eef7
mosh-exe <mohammeds@mun.ca>
2021-03-06 18:04:49 -0330

    further modification to addevent
    Signed-off-by: mosh-exe <mohammeds@mun.ca>

---
+++ src/main/java/org/openjfx/AddEventController.java
@@ -0,0 +1,14 @@
+/*
+@Author Mohammed Shahwan
+ */
+
+package org.openjfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class AddEventController {
+
+    @FXML
+    void Quit(MouseEvent event) { System.exit(0);}
+}
