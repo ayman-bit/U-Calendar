28f496a057c4e80523241b837127ad22804ac249
mosh-exe <mohammeds@mun.ca>
2021-03-22 23:34:39 -0230

    Removed unused code from Task_Controller
    Signed-off-by: mosh-exe <mohammeds@mun.ca>

--- src/main/java/org/openjfx/Tasks_Controller.java
+++ src/main/java/org/openjfx/Tasks_Controller.java
@@ -4,18 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 import java.util.*;
 
 /**
@@ -23,8 +19,7 @@ import java.util.*;
  */
 
 public class Tasks_Controller implements Initializable {
-
-
+    
     @FXML
     private TableView<Events> table;
 
@@ -40,8 +35,6 @@ public class Tasks_Controller implements Initializable {
     @FXML
     private TableColumn<Events, String> timeCol;
 
-
-
     @FXML
     void back(MouseEvent event) throws IOException {
     Controller.start("Application.fxml",event);
@@ -49,19 +42,16 @@ public class Tasks_Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        IdCol.setCellValueFactory(new PropertyValueFactory<Events,Integer>("id"));
-        nameCol.setCellValueFactory(new PropertyValueFactory<Events,String>("name"));
-        dateCol.setCellValueFactory(new PropertyValueFactory<Events,String>("date"));
-        timeCol.setCellValueFactory(new PropertyValueFactory<Events,String>("time"));
+        IdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        timeCol.setCellValueFactory(new PropertyValueFactory<>("time"));
 
         List<Map<String, Object>> QU = DatabaseHandler.execQuery("SELECT * FROM userData");
-
-
         ObservableList<Events> list = FXCollections.observableArrayList();
 
-
-        for(int i = 0; i < QU.size(); i++)
-        {
+        assert QU != null;
+        for (Map<String, Object> stringObjectMap : QU) {
             Calendar c = Calendar.getInstance();
             // set the calendar to start of today
             c.set(Calendar.HOUR_OF_DAY, 0);
@@ -71,8 +61,7 @@ public class Tasks_Controller implements Initializable {
             // and get that as a Date
             Date today = c.getTime();
 
-            Map<String, Object> eventsFromDB = QU.get(i);
-            String[] dates = eventsFromDB.get("date").toString().split("-");
+            String[] dates = stringObjectMap.get("date").toString().split("-");
 
             // let's say the components come from a form or something
             int year = Integer.parseInt(dates[0]);
@@ -81,19 +70,15 @@ public class Tasks_Controller implements Initializable {
 
             // reuse the calendar to set user specified date
             c.set(Calendar.YEAR, year);
-            //added the negative 1 because all month was one ahead not sure why excatly
-            c.set(Calendar.MONTH, month-1);
+            //added the negative 1 because all month was one ahead not sure why exactly
+            c.set(Calendar.MONTH, month - 1);
             c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
             Date dateSpecified = c.getTime();
 
-            System.out.println(today);
-            System.out.println(dateSpecified);
-            System.out.println(dates[0] + "," + dates[1] + "'" + dates[2]);
-
             //Check if events occurs after "today"
-            if(dateSpecified.after(today)) {
-                list.add(new Events((Integer) eventsFromDB.get("id"), eventsFromDB.get("eventName").toString(), eventsFromDB.get("date").toString(), eventsFromDB.get("startTime").toString()));
+            if (dateSpecified.after(today)) {
+                list.add(new Events((Integer) stringObjectMap.get("id"), stringObjectMap.get("eventName").toString(), stringObjectMap.get("date").toString(), stringObjectMap.get("startTime").toString()));
             }
         }
         table.setItems(list);
