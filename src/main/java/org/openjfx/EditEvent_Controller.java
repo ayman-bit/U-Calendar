175f679532885aa596debb763657cb0c6fe01def
mosh-exe <mohammeds@mun.ca>
2021-03-23 21:57:23 -0230

    Fixed some visual discrepancy with addevent window. Implemented EditEvent. Implemented DeleteEvent.
    Signed-off-by: mosh-exe <mohammeds@mun.ca>

--- src/main/java/org/openjfx/EditEvent_Controller.java
+++ src/main/java/org/openjfx/EditEvent_Controller.java
@@ -3,28 +3,28 @@ package org.openjfx;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTabPane;
 import com.jfoenix.controls.JFXTextField;
-        import com.jfoenix.controls.JFXTimePicker;
-        import java.io.IOException;
-        import java.net.URL;
-        import java.time.LocalDate;
-        import java.time.LocalTime;
-        import java.util.ResourceBundle;
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Node;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.control.Alert;
-        import javafx.scene.input.MouseEvent;
-        import javafx.stage.Stage;
+import com.jfoenix.controls.JFXTimePicker;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.*;
 
 /**
- * @author Ayman Abu Awad
  * @author Mohammed Shahwan
  */
 
-public class AddEvent_Controller {
+public class EditEvent_Controller {
+
+    List<Map<String, Object>> QU = DatabaseHandler.execQuery("SELECT * FROM userData");
 
     @FXML
     private JFXDatePicker date;
@@ -60,7 +60,10 @@ public class AddEvent_Controller {
     private JFXTimePicker FinalEndTime;
 
     @FXML
-    void Next(MouseEvent e) throws IOException{
+    private ChoiceBox<String> eventMenu;
+
+    @FXML
+    void Next(MouseEvent e) throws IOException {
         tabPane.getSelectionModel().selectNext();
     }
 
@@ -71,7 +74,7 @@ public class AddEvent_Controller {
 
     @FXML
     void Cancel(MouseEvent event) throws IOException {
-        Controller.start("Application.fxml" ,event);
+        Controller.start("Application.fxml",event);
     }
 
     @FXML
@@ -110,14 +113,13 @@ public class AddEvent_Controller {
                     + "'" + endFT + "',"
                     + "'" + Login_Controller.uid + "'"
                     + ")";
-            System.out.println(qu);
 
             if(DatabaseHandler.execAction(qu)){ //Success
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setHeaderText(null);
                 alert.setContentText("Success");
                 alert.showAndWait();
-                Controller.start("Application.fxml", event);
+                Controller.start("Application.fxml",event);
             }
             else{ // Error
                 Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -126,7 +128,7 @@ public class AddEvent_Controller {
                 alert.showAndWait();
             }
 
-            }
+        }
         catch (Exception e){
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText(null);
@@ -138,11 +140,62 @@ public class AddEvent_Controller {
     }
 
     @FXML
+    void NextPopulate(MouseEvent event) throws IOException, ParseException {
+        DateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        assert QU != null;
+        for (Map<String, Object> stringObjectMap : QU) {
+            if(stringObjectMap.get("eventName").toString().equals(eventMenu.getValue())) {
+                System.out.println(eventMenu.getValue());
+                className.setText(stringObjectMap.get("eventName").toString());
+                Date temp = simpleDateFormat.parse(stringObjectMap.get("date").toString());
+                date.setValue(convertToLocalDateViaInstant(temp));
+                startTime.setValue(LocalTime.parse(stringObjectMap.get("startTime").toString()));
+                endTime.setValue(LocalTime.parse(stringObjectMap.get("endTime").toString()));
+                numAssign.setText(stringObjectMap.get("numAssign").toString());
+                numTests.setText(stringObjectMap.get("numTest").toString());
+                numLabs.setText(stringObjectMap.get("numLabs").toString());
+                temp = simpleDateFormat.parse(stringObjectMap.get("finalDate").toString());
+                FinalDate.setValue(convertToLocalDateViaInstant(temp));
+                FinalStartTime.setValue(LocalTime.parse(stringObjectMap.get("finalStartTime").toString()));
+                FinalEndTime.setValue(LocalTime.parse(stringObjectMap.get("finalEndTime").toString()));
+
+                //After extracting the needed data delete event from database
+                String qu = "DELETE FROM userData WHERE eventName=" + "'"+stringObjectMap.get("eventName").toString()+"'";
+                DatabaseHandler.execAction(qu);
+            }
+        }
+        tabPane.getSelectionModel().selectNext();
+    }
+
+
+    @FXML
     void initialize() {
         assert date != null : "fx:id=\"date\" was not injected: check your FXML file 'AddEvent.fxml'.";
         assert startTime != null : "fx:id=\"startTime\" was not injected: check your FXML file 'AddEvent.fxml'.";
         assert endTime != null : "fx:id=\"endTime\" was not injected: check your FXML file 'AddEvent.fxml'.";
         assert className != null : "fx:id=\"className\" was not injected: check your FXML file 'AddEvent.fxml'.";
+        populateChoice();
+    }
 
+    // Find all user events
+    void populateChoice(){
+        ArrayList<String> dates = new ArrayList<String>();
+        assert QU != null;
+        for (Map<String, Object> stringObjectMap : QU) {
+              dates.add(stringObjectMap.get("eventName").toString());
+        }
+
+        for(String q: dates)
+        {
+            eventMenu.getItems().add(q);
+        }
+    }
+
+    //converting Date to LocalDate using instant
+    public LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
     }
 }
+
